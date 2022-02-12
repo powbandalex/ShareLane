b2832a43ad0389c0a26a525f16f0902db1c6bcab
Aliaksey Khadasevich <akhadasevich@powerbandsolutions.com>
2022-02-12 15:11:59 +0300

    Lesson 9

--- src/test/java/utilities/GenerateFakeMessage.java
+++ src/test/java/utilities/GenerateFakeMessage.java
@@ -13,4 +13,24 @@ public class GenerateFakeMessage {
         Faker faker = new Faker();
         return faker.numerify("23####");
     }
+
+    public static String getFirstName() {
+        Faker faker = new Faker();
+        return faker.name().firstName();
+    }
+
+    public static String getLastName() {
+        Faker faker = new Faker();
+        return faker.name().lastName();
+    }
+
+    public static String getEmail() {
+        Faker faker = new Faker();
+        return faker.internet().emailAddress();
+    }
+
+    public static String getPassword() {
+        Faker faker = new Faker();
+        return faker.numerify("string@##");
+    }
 }
