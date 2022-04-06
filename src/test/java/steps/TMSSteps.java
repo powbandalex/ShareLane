729f13c73500e9ee1de63a4d1f84322e60961fa3
Aliaksey Khadasevich <akhadasevich@powerbandsolutions.com>
2022-04-06 10:10:16 +0300

    Cucumber added

--- src/test/java/steps/TMSSteps.java
+++ src/test/java/steps/TMSSteps.java
@@ -10,6 +10,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.util.concurrent.TimeUnit;
 
 public class TMSSteps {
 
@@ -19,6 +22,8 @@ public class TMSSteps {
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
     @After
@@ -39,11 +44,14 @@ public class TMSSteps {
 
     @And("User click {string} button")
     public void userClickButton(String buttonName) {
-        String buttonXpath = String.format("//div[contains(@class, 'animated-btn')]//div[contains(., '%s')]", buttonName);
+        String buttonXpath = String.format("//div[contains(@class, 'animated-btn')]//div[contains(., '%s')]//ancestor::div[contains(@class, 'animation')]", buttonName);
         driver.findElement(By.xpath(buttonXpath)).click();
     }
 
     @Then("{string} option presents and is first in the list")
-    public void optionPresentsAndIsFirstInTheList(String arg0) {
+    public void optionPresentsAndIsFirstInTheList(String courseName) {
+        String courseNameXpath = "(//div[contains(@class, 't-name_lg')])[1]";
+        String actualResult = driver.findElement(By.xpath(courseNameXpath)).getText().replace("\n", " ");
+        Assert.assertEquals(courseName, actualResult, String.format("%s course isn't shown as first option", courseName));
     }
 }
--- src/test/resources/features/teachmeskillbdd.feature
+++ src/test/resources/features/teachmeskillbdd.feature
@@ -1,6 +1,20 @@
 Feature: Search course
+
+  @qa
+  Scenario Outline: User searches for course
+    Given User is on main screen
+    When User clicks "Курсы" tab
+    And User click <course> button
+    Then "Front End разработчик" option presents and is first in the list
+    Examples:
+      | course         |
+      | "Офлайн курсы" |
+      | "Онлайн-курсы" |
+
+
+  @prod
   Scenario: User searches for course
     Given User is on main screen
     When User clicks "Курсы" tab
     And User click "Офлайн курсы" button
-    Then "Front End разработчик" option presents and is first in the list
+    Then "Python разработчик" option presents and is first in the list
